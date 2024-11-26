22bb08269df9c56942a8d88d0c9b498cfc8bedfd
Andrii Borovskyi <abrvsk@amazon.com>
2024-11-26 20:28:29 +0000

    Added mini.nvim for mini.files plugin. TODO: check other mini plugins for more features, like pairs and jumps. Add remaps to reload the config and resize splits faster. Enable Hop jumps in netrw (see remaps in /after/fast_navigation.lua) Updated ripgrep plugin in telescope.
    Added Elixir tools.
    Updated gruvbox colors to match Elixir theme better.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,9 +4,10 @@ vim.keymap.set('n', '<leader>t', ':Telescope<cr>', {})
 vim.keymap.set('n', '<C-f>', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
-vim.keymap.set('n', '<C-s>', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<C-s>', ':Rg ')
+-- vim.keymap.set('n', '<C-s>', function()
+-- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+-- end)
 
 local actions = require("telescope.actions")
 require("telescope").setup {
